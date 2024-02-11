af4207f35b1fcc7e58608d6e8073452314a21c52
pauloch8 <paulo.ch8@gmail.com>
2024-02-11 19:47:57 +0000

    resolução do primeiro desafio

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hello and welcome to the C++" << std::endl;
+    std::cout << "Please, enter your name: " << std::flush;
+    std::string str;
+    std::cin >> str;
+    std:: cout << "Hello, " << str << ". Wecome to our course" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
