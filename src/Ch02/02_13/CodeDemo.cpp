6ab4d5ea3944042f46083df6fa560b2effd188e5
pauloch8 <paulo.ch8@gmail.com>
2024-02-12 07:15:33 +0000

    Challange 2

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -5,8 +5,9 @@
 #include <iostream>
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
-    float result; 
+    const int nums[5] = {1,23,32,24,337};
+    const int sum = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+    const float result = sum / 5.0f;
 
     // Write your code here
     
