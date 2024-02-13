20bbe4661831e40abee2fb4d96941dce2b59b450
pauloch8 <paulo.ch8@gmail.com>
2024-02-13 18:14:43 +0000

    if statements

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,28 @@
 #include <iostream>
 
 int main(){
-    
+    int a = 1024;
+    if(a > 1000){
+        std::cout << "Warning: a is over 1000." << std::endl;
+    }
+    if((a % 2) == 0) {
+        std::cout << "A is odd";
+    } else {
+        std::cout << "A is even";
+    }
     std::cout << std::endl << std::endl;
+
+    char lttr = 'd';
+    std::cout << "The letter " << lttr;
+    const bool isLowerVowel = lttr == 'a' || lttr == 'e' || lttr == 'i' || lttr == 'o' || lttr == 'u';
+    const bool isUpperVowel = lttr == 'A' || lttr == 'E' || lttr == 'I' || lttr == 'O' || lttr == 'U';
+    if(!isLowerVowel && !isUpperVowel){
+        std::cout << " is not ";
+    }
+    else {
+        std::cout << " is ";
+    }
+    std::cout << "a vowel" << std::endl;
+
     return (0);
 }
