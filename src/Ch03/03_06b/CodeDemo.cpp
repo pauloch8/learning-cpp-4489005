def3f4d9a48a5ba392595a3bfadffc72cda8e05e
pauloch8 <paulo.ch8@gmail.com>
2024-02-13 06:48:55 +0000

    Pointers

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -5,7 +5,17 @@
 #include <iostream>
 #include <string>
 
+// Smart Pointers é uma alternativa moderna e segura para Pointers
+
 int main(){
+    int a = 37;
+    int *ptr = &a;
+
+    std::cout << "O conteudo de a é: " << a << std::endl;
+    std::cout << "O endereço de a é: " << &a << std::endl;
+    std::cout << "O endereço contido em ptr é: " << ptr << std::endl;
+    std::cout << "O valor contido no endereço que ptr aponta é: " << *ptr << std::endl;
+    std::cout << "O endereço do pointer ptr é: " << &ptr << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
