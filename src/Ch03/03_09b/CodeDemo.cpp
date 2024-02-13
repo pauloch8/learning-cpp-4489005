5be231e12f5ac7e47b552b4c5671156f506c3a30
pauloch8 <paulo.ch8@gmail.com>
2024-02-13 15:16:02 +0000

    Using objects with vectors

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -2,10 +2,26 @@
 // Exercise 03_09
 // Vectors, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
+    std::vector<int> primes;
+
+    std::cout << "The vector has " << primes.size() << " elements."<< std::endl;
+
+    primes.push_back(2);
+    primes.push_back(3);
+    primes.push_back(4);
+    primes.push_back(5);
+    primes.push_back(7);
+    primes.push_back(11);
+
+    std::cout << "After adding, the vector has " << primes.size() << " elements."<< std::endl;
+    std::cout << "The element in index 2 is " << primes[2] << std::endl;
+    primes[2] = 13;
+    std::cout << "After rewrite, the element in index 2 is " << primes[2] << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
