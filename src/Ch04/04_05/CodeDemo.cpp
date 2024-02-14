f79cd7fb4c2e3c43eb0c4625e432e2bf6f312283
pauloch8 <paulo.ch8@gmail.com>
2024-02-14 02:04:04 +0000

    Challange 4: minha primeira solução

--- src/Ch04/04_05/CodeDemo.cpp
+++ src/Ch04/04_05/CodeDemo.cpp
@@ -26,6 +26,70 @@ int main(){
 
     // Calculate the GPA for the selected student.
     // Write your code here
+    Student* selectedStudent;
+    for(int i=0; i < students.size(); i++){
+        Student student = students[i];
+        if(student.get_id() == id){
+            selectedStudent = &student;
+            break;
+        }
+    }
+    if(!selectedStudent){
+        std::cout << "Error: Estudant id '" << id << "' not found";
+        std::cout << std::endl << std::endl;
+        return 1;
+    }
+
+    float totalPoints = 0.0f;
+    int totalCredits = 0;
+    for(int i=0; i < grades.size(); i++){
+        Grade grade = grades[i];
+        int gradeStudentId = grade.get_student_id();
+        if(gradeStudentId != id){
+            continue;
+        }
+        int credits;
+        for(int j=0; j < courses.size(); j++){
+            Course course = courses[j];
+            if(course.get_id() != grade.get_course_id()){
+                continue;
+            }
+            credits = course.get_credits();
+            break;
+        }
+        if(!credits){
+            std::cout << "Error: Grade id '" << gradeStudentId << "' not found";
+            std::cout << std::endl << std::endl;
+            return 1;
+        }
+        char gradeValue = grade.get_grade();
+        int gradePoints;
+        switch(gradeValue){
+            case 'A':
+                gradePoints = 4;
+                break;
+            case 'B':
+                gradePoints = 3;
+                break;
+            case 'C':
+                gradePoints = 2;
+                break;
+            case 'D':
+                gradePoints = 1;
+                break;
+            case 'F':
+                gradePoints = 0;
+                break;
+            default:
+                std::cout << "Error: GradeValue '" << gradeValue << "' not found";
+                std::cout << std::endl << std::endl;
+                return 1;
+                break;
+        }
+        totalPoints += credits * gradePoints;
+        totalCredits += credits;
+    }
+    GPA = totalPoints / totalCredits;
 
     std::string student_str;
     student_str = students[0].get_name(); // Change this to the selected student's name
